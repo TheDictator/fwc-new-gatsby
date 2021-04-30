87891e21e51a5dfbb447b1c257144270c433f65c
Kevin Carpenter <carpenter.kevin@gmail.com>
2021-04-30 19:30:32 +0700

    hero for blog

--- src/templates/BlogPosts.tsx
+++ src/templates/BlogPosts.tsx
@@ -50,6 +50,23 @@ export const BlogPostsPage = (props: Props) => {
 	return (
 		<Layout location={props.location}>
 			<SEO title={`${site.siteMetadata.title} | ${site.siteMetadata.description}`} description={site.siteMetadata.description} />
+			<div className="relative bg-indigo-800">
+      <div className="absolute inset-0">
+        <img
+          className="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
+          alt=""
+        />
+        <div className="absolute inset-0 bg-blue-800" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
+      </div>
+      <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+        <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">The Blog</h1>
+        <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+          Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel
+          lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.
+        </p>
+      </div>
+    </div>
 			<div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
 					{group.map(({ node }: { node: Post }) => {
 						const fluid: FluidObject | null = (node.featured_media && node.featured_media.localFile && node.featured_media.localFile.childImageSharp && node.featured_media.localFile.childImageSharp.fluid) ? node.featured_media.localFile.childImageSharp.fluid : null;
