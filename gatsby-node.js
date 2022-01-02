5fc44d1a3c3e6cd75597149ec5a932f13eca3f3f
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-01-02 15:39:07 +0700

    Fix blog tag pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,189 +1,195 @@
-const moment = require('moment');
-const path = require('path');
+const moment = require("moment")
+const path = require("path")
 
-const createPaginatedPages = require('gatsby-paginate');
-const paginate = require('gatsby-awesome-pagination');
+const createPaginatedPages = require("gatsby-paginate")
+const paginate = require("gatsby-awesome-pagination")
 
-require('dotenv').config({
-	path: `.env.${process.env.NODE_ENV}`
-});
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
-exports.createPages = async ({
-	graphql,
-	actions,
-	reporter
-}) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
+    const { createPage } = actions
 
-	const {
-		createPage
-	} = actions;
+    const BlogPostTemplate = path.resolve("./src/templates/BlogPost.tsx")
+    const BlogTagPostsTemplate = path.resolve(
+        "./src/templates/BlogTagPosts.tsx"
+    )
+    const BlogCategoryPostsTemplate = path.resolve(
+        "./src/templates/BlogCategoryPosts.tsx"
+    )
 
-	const BlogPostTemplate = path.resolve("./src/templates/BlogPost.tsx");
-	const BlogTagPostsTemplate = path.resolve("./src/templates/BlogTagPosts.tsx");
-	const BlogCategoryPostsTemplate = path.resolve("./src/templates/BlogCategoryPosts.tsx");
+    const BlogPostsResult = await graphql(`
+        {
+            allWpPost {
+                edges {
+                    node {
+                        id
+                        slug
+                        uri
+                        link
+                        title
+                        excerpt
+                        date(formatString: "MMMM DD, YYYY")
+                        modified(formatString: "MMMM DD, YYYY")
+                        author {
+                            node {
+                                avatar {
+                                    url
+                                }
+                                id
+                                name
+                                uri
+                                slug
+                            }
+                        }
+                        featuredImage {
+                            node {
+                                localFile {
+                                    childImageSharp {
+                                        gatsbyImageData(
+                                            width: 1920
+                                            placeholder: BLURRED
+                                            formats: [AUTO, WEBP, AVIF]
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        categories {
+                            nodes {
+                                id
+                                count
+                                name
+                                slug
+                            }
+                        }
+                        tags {
+                            nodes {
+                                id
+                                count
+                                name
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `)
 
-	const BlogPostsResult = await graphql(`
-	{
-		allWpPost {
-			edges {
-			  node {
-				id
-				slug
-				uri
-				link
-				title
-				excerpt
-				date(formatString: "MMMM DD, YYYY")
-				modified(formatString: "MMMM DD, YYYY")
-				author {
-				  node {
-					avatar {
-						url
-					}
-					id
-					name
-					uri
-					slug
-				  }
-				}
-				featuredImage {
-				  node {
-					localFile {
-					  childImageSharp {
-						gatsbyImageData (
-                            width: 1920
-                            placeholder: BLURRED
-                            formats: [AUTO, WEBP, AVIF]
-                          )
-					  }
-					}
-				  }
-				}
-				categories {
-				  nodes {
-					id
-					count
-					name
-					slug
-				  }
-				}
-				tags {
-				  nodes {
-					id
-					count
-					name
-					slug
-				  }
-				}
-			  }
-			}
-		  }
-	}`);
+    if (BlogPostsResult.errors) {
+        reporter.panicOnBuild("Error while running GraphQL query.")
+        return
+    }
 
-	if (BlogPostsResult.errors) {
-		reporter.panicOnBuild('Error while running GraphQL query.');
-		return;
-	}
+    const BlogPosts = BlogPostsResult.data.allWpPost.edges
 
-	const BlogPosts = BlogPostsResult.data.allWpPost.edges;
+    BlogPosts.forEach((post, index) => {
+        const date = post.node.date
+        createPage({
+            path: `/${post.node.categories.nodes[0].slug}/${moment(date).format(
+                "YYYY"
+            )}/${moment(date).format("MM")}/${post.node.slug}.html`,
+            component: BlogPostTemplate,
+            context: {
+                id: post.node.id,
+                slug: post.node.slug,
+                uri: post.node.uri,
+                previous: index === 0 ? null : BlogPosts[index - 1].node,
+                next:
+                    index === BlogPosts.length - 1
+                        ? null
+                        : BlogPosts[index + 1].node,
+            },
+        })
+    })
 
-	BlogPosts.forEach((post, index) => {
-		const date = post.node.date;
-		createPage({
-			path: `/${post.node.categories.nodes[0].slug}/${moment(date).format('YYYY')}/${moment(date).format('MM')}/${post.node.slug}.html`,
-			component: BlogPostTemplate,
-			context: {
-				id: post.node.id,
-				slug: post.node.slug,
-				uri: post.node.uri,
-				previous: index === 0 ? null : BlogPosts[index - 1].node,
-				next: index === (BlogPosts.length - 1) ? null : BlogPosts[index + 1].node
-			}
-		});
-	});
+    createPaginatedPages({
+        edges: BlogPosts,
+        createPage: createPage,
+        pageTemplate: "src/templates/BlogPosts.tsx",
+        pageLength: 10,
+        pathPrefix: "blog",
+    })
 
-	createPaginatedPages({
-		edges: BlogPosts,
-		createPage: createPage,
-		pageTemplate: 'src/templates/BlogPosts.tsx',
-		pageLength: 10,
-		pathPrefix: 'blog',
-	});
-	// paginate({
-	// 	createPage,
-	// 	items: BlogPosts,
-	// 	itemsPerPage: 10,
-	// 	pathPrefix: 'blog',
-	// 	component: 'src/templates/BlogPosts.tsx', // your template for post lists
-	//   })
+    const BlogTagPosts = new Map()
+    const BlogCategoryPosts = new Map()
 
-	const BlogTagPosts = new Map();
-	const BlogCategoryPosts = new Map();
+    BlogPosts.forEach((post) => {
+        const tags = post.node.tags.nodes
+        if (tags && tags.length > 0) {
+            tags.forEach((tag) => {
+                if (BlogTagPosts.has(tag.slug)) {
+                    BlogTagPosts.set(tag.slug, [
+                        ...BlogTagPosts.get(tag.slug),
+                        post,
+                    ])
+                } else {
+                    BlogTagPosts.set(tag.slug, [post])
+                }
+                if (BlogTagPosts.has(tag.title)) {
+                    BlogTagPosts.set(tag.title, [
+                        ...BlogTagPosts.get(tag.title),
+                        post,
+                    ])
+                } else {
+                    BlogTagPosts.set(tag.title, [post])
+                }
+            })
+        }
+        const categories = post.node.categories.nodes
+        if (categories && categories.length > 0) {
+            categories.forEach((category) => {
+                if (BlogCategoryPosts.has(category.slug)) {
+                    BlogCategoryPosts.set(category.slug, [
+                        ...BlogCategoryPosts.get(category.slug),
+                        post,
+                    ])
+                } else {
+                    BlogCategoryPosts.set(category.slug, [post])
+                }
+                if (BlogCategoryPosts.has(category.title)) {
+                    BlogCategoryPosts.set(category.title, [
+                        ...BlogCategoryPosts.get(category.title),
+                        post,
+                    ])
+                } else {
+                    BlogCategoryPosts.set(category.title, [post])
+                }
+            })
+        }
+    })
 
-	BlogPosts.forEach((post) => {
-		const tags = post.node.tags;
-		if (tags && tags.length > 0) {
-			tags.forEach((tag) => {
-				if (BlogTagPosts.has(tag.slug)) {
-					BlogTagPosts.set(tag.slug, [...BlogTagPosts.get(tag.slug), post]);
-				} else {
-					BlogTagPosts.set(tag.slug, [post]);
-				}
-				if (BlogTagPosts.has(tag.title)) {
-					BlogTagPosts.set(tag.title, [...BlogTagPosts.get(tag.title), post]);
-				} else {
-					BlogTagPosts.set(tag.title, [post]);
-				}
-			});
-		}
-		const categories = post.node.categories.nodes;
-		if (categories && categories.length > 0) {
-			categories.forEach((category) => {
-				if (BlogCategoryPosts.has(category.slug)) {
-					BlogCategoryPosts.set(category.slug, [...BlogCategoryPosts.get(category.slug), post]);
-				} else {
-					BlogCategoryPosts.set(category.slug, [post]);
-				}
-				if (BlogCategoryPosts.has(category.title)) {
-					BlogCategoryPosts.set(category.title, [...BlogCategoryPosts.get(category.title), post]);
-				} else {
-					BlogCategoryPosts.set(category.title, [post]);
-				}
-			});
-		}
-	});
+    const BlogTagSlugs = [...BlogTagPosts.keys()]
+    const BlogCategorySlugs = [...BlogCategoryPosts.keys()]
+    //const BlogCategoryTitles = [...BlogCategoryPosts.keys()];
 
-	const BlogTagSlugs = [...BlogTagPosts.keys()];
-	const BlogCategorySlugs = [...BlogCategoryPosts.keys()];
-	const BlogCategoryTitles = [...BlogCategoryPosts.keys()];
+    if (BlogTagSlugs.length > 0) {
+        BlogTagSlugs.forEach((BlogTagSlug) => {
+            createPage({
+                path: `/tag/${BlogTagSlug}`,
+                component: BlogTagPostsTemplate,
+                context: {
+                    group: BlogTagPosts.get(BlogTagSlug),
+                    slug: BlogTagSlug,
+                    title: BlogTagPosts.get("tag.title"),
+                },
+            })
+        })
+    }
 
-	if (BlogTagSlugs.length > 0) {
-		BlogTagSlugs.forEach((BlogTagSlug) => {
-			createPage({
-				path: `/tag/${BlogTagSlug}`,
-				component: BlogTagPostsTemplate,
-				context: {
-					group: BlogTagPosts.get(BlogTagSlug),
-					slug: BlogTagSlug,
-					title: BlogTagPosts.get('tag.title')
-				}
-			});
-		});
-	}
-
-	if (BlogCategorySlugs.length > 0) {
-		BlogCategorySlugs.forEach((BlogCategorySlug) => {
-			createPage({
-				path: `/category/${BlogCategorySlug}`,
-				component: BlogCategoryPostsTemplate,
-				context: {
-					group: BlogCategoryPosts.get(BlogCategorySlug),
-					slug: BlogCategorySlug,
-					title: BlogCategoryPosts.get('category.title')
-				}
-			});
-		});
-	}
-
-	
+    if (BlogCategorySlugs.length > 0) {
+        BlogCategorySlugs.forEach((BlogCategorySlug) => {
+            createPage({
+                path: `/category/${BlogCategorySlug}`,
+                component: BlogCategoryPostsTemplate,
+                context: {
+                    group: BlogCategoryPosts.get(BlogCategorySlug),
+                    slug: BlogCategorySlug,
+                    title: BlogCategoryPosts.get("category.title"),
+                },
+            })
+        })
+    }
 }
--- src/templates/BlogTagPosts.tsx
+++ src/templates/BlogTagPosts.tsx
@@ -1,58 +1,76 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
-import { wpPost } from '../contracts/post';
-import { capitalizeFirstLetter } from '../utils';
-import '../styles/blog.scss';
-import PostCards from "../components/Posts/cards";
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import { wpPost } from "../contracts/post"
+import { capitalizeFirstLetter } from "../utils"
+import "../styles/blog.scss"
+import PostCards from "../components/Posts/cards"
 import Cta from "../components/Ui/Cta"
-
 export interface Props {
-	pathContext: {
-		group: { node: wpPost }[];
-		slug: string;
-	};
-	location: Location;
+    pageContext: {
+        group: { wpPost: wpPost }[]
+        slug: string
+        uri: string
+        title: string
+    }
+    location: Location
 }
 
 export const BlogTagPostsPage = (props: Props) => {
-	const { group } = props.pathContext;
-	const { site } = useStaticQuery(graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-          }
+    const { group } = props.pageContext
+    const { site } = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                    description
+                }
+            }
         }
-      }
-	`);
-	return (
-		<Layout location={props.location}>
-			<SEO title={`${site.siteMetadata.title} | ${site.siteMetadata.description}`} description={site.siteMetadata.description} />
-			<div className="relative bg-gray-500">
-				<div className="absolute inset-0">
-					<div className="absolute inset-0 bg-gray-500" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
-				</div>
-				<div className="relative max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
-					<span className="block bg-gradient-to-r from-blue-300 to-blue-300 bg-clip-text text-transparent text-base font-semibold tracking-wide uppercase">Blog</span>
-					<h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl m-0">Browsing By Tag: <span className="bg-gradient-to-r from-blue-300 to-blue-300 bg-clip-text text-transparent">{capitalizeFirstLetter(props.pathContext.slug)}</span></h1>
-				</div>
-			</div>
-			<div className="relative max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-				<div id="primary" className="content-area with-sidebar col-span-1 md:col-span-12">
-					<div className="posts mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none xl:grid-cols-3">
-						<PostCards posts={group} />
-					</div>
-				</div>
-			</div>
-			<Cta
+    `)
+    return (
+        <Layout location={props.location}>
+            <SEO
+                title={`${site.siteMetadata.title} | ${site.siteMetadata.description}`}
+                description={site.siteMetadata.description}
+            />
+            <div className="relative bg-gray-500">
+                <div className="absolute inset-0">
+                    <div
+                        className="absolute inset-0 bg-gray-500"
+                        style={{ mixBlendMode: "multiply" }}
+                        aria-hidden="true"
+                    />
+                </div>
+                <div className="relative max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
+                    <span className="block bg-gradient-to-r from-blue-300 to-blue-300 bg-clip-text text-transparent text-base font-semibold tracking-wide uppercase">
+                        Blog
+                    </span>
+                    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl m-0">
+                        Browsing By Tag:{" "}
+                        <span className="bg-gradient-to-r from-blue-300 to-blue-300 bg-clip-text text-transparent">
+                            {capitalizeFirstLetter(props.pageContext.slug)}
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className="relative max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+                <div
+                    id="primary"
+                    className="content-area with-sidebar col-span-1 md:col-span-12"
+                >
+                    <div className="posts mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none xl:grid-cols-3">
+                        <PostCards posts={group} />
+                    </div>
+                </div>
+            </div>
+            <Cta
                 headline="Ready to solve your woes?"
                 description="Contact Us. The first hour is free."
             />
-		</Layout>
-	);
-};
+        </Layout>
+    )
+}
 
-export default BlogTagPostsPage;
+export default BlogTagPostsPage
