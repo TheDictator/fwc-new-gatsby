4a821cb6f8ef3c9c95b53bb53a2b35631b16868c
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-01-09 13:35:31 +0700

    Remove file

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import SEO from '../../components/SEO';
 import '../../styles/blog.scss';
-import Cta from "../../components/Ui/Cta"
-
+import Cta from "../../components/Ui/Cta";
 const faqs = [
 	{
 	  id: 1,
